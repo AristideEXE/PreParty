e40bd8cf02b18f7d20d8e79d537a8dc767290041
Aristide Proriol <aristidepr@gmail.com>
2022-10-24 09:17:08 +0200

    Ajoute la page d'affichage de fête

---
+++ PreParty/Pages/fete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PreParty.Pages
+{
+    public class feteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
